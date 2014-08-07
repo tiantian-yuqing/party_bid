38a0e434155f79481dab826b12a18e9e608f7134
tiantian-yuqing <501873986@qq.com>
2014-08-07 11:23:27 -0400

    简化判断是否重复和存储的逻辑

--- app/model/activity.js
+++ app/model/activity.js
@@ -1,11 +1,11 @@
 function Activity( activity_name) {
     this.name = activity_name;
 }
-                                                             //存储输入的活动名称
+                                                                         //存储输入的活动名称
 Activity.prototype.save=function(){
     localStorage.setItem("activity_name", this.name);
 };
-                                                              //返回输入的活动名称,供其他页面使用
+                                                                        //返回输入的活动名称,供其他页面使用
 Activity.get_activity_name = function () {
     return localStorage.getItem('activity_name');
 };
@@ -18,23 +18,14 @@ Activity.get_all_activities_json=function(){
     return JSON.parse(localStorage.getItem('activities_arr'))
 };
                                                                           //判断重复
-Activity.judge_duplicate=function(activity_name){
-    for (var i = 0 ; i < (Activity.get_all_activities_json()).length ; i++){
-        //alert("b");
-        var activities_arr = Activity.get_all_activities_json();
-        if(activity_name == activities_arr[i].name){
-            return true ;
-        }
+Activity.judge_duplicate=function(activity_name,activities_arr){
+    for (var i = 0 ; i < (activities_arr.length) ; i++){
+        return (activity_name == activities_arr[i].name);
     }
 };
 
-Activity.localStorage_activity1=function(activities_arr,activity1){                         //存储数组到localstorage
+Activity.localStorage_activity1=function(activities_arr,activity1){                         //存储实例到localstorage
     activities_arr.unshift(activity1);
     localStorage.setItem('activities_arr', JSON.stringify(activities_arr));
 };
 
-//Activity.put_activity_to_localstorage=function(){
-
-//}
-//activities_arr.unshift(activity1);
-//Activity.localStorage_activity(activities_arr);
--- app/scripts/controllers/create_activity_controller.js
+++ app/scripts/controllers/create_activity_controller.js
@@ -1,15 +1,29 @@
 'use strict';
 
 angular.module('testApp')
-    .controller('createActivityCtrl', function ($scope, $location)
-    {
+    .controller('createActivityCtrl', function ($scope, $location) {
         $scope.back = "返回";
         $scope.create_activity = "创建";
-        $scope.show =! Activity.judge_activities_arr_empty();
-        $scope.back_to_activity_list = function () {$location.path('/activity_list')};
-        $scope.go_activity_sign_up = function (){
+        $scope.show = !Activity.judge_activities_arr_empty();
+        $scope.back_to_activity_list = function () {
+               $location.path('/activity_list')
+        };
+        $scope.go_activity_sign_up = function () {
             var activity1 = new Activity($scope.activity_name);
-            var activities_arr = Activity.get_all_activities_json()||[];
+            var activities_arr = Activity.get_all_activities_json() || [];
+            alert(activities_arr.length);
+            if (Activity.judge_duplicate($scope.activity_name,activities_arr)) {
+                $scope.warning = true;
+            }
+            else {
+                Activity.localStorage_activity1(activities_arr, activity1);
+                $location.path('/activity_sign_up')
+            }
+
+        }
+    })
+
+        /*
             if ( Activity.judge_activities_arr_empty() ){
                  Activity.localStorage_activity1(activities_arr,activity1);
                  $location.path('/activity_sign_up')
@@ -26,4 +40,4 @@ angular.module('testApp')
         }
     })
 
-
+*/
