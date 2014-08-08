e7b2721cd7ecefbee2e8612bd78988bc6e2890e1
tiantian-yuqing <501873986@qq.com>
2014-08-07 23:54:11 -0400

    简化开始（结束）按钮的显示

--- app/model/message.js
+++ app/model/message.js
@@ -11,11 +11,28 @@ Message.click_start = function(){
 
 };
 
-Message.click_end = function(count){
+Message.click_end = function(){
         return(confirm("是否结束本次报名?"));
 };
 
-Message.judge_count=function(count) {
+Message.judge_count = function(count) {
     return((count == 0)?"zero" :(count % 2 == 0) ? "even" : "odd");
 };
 
+Message.button = function (count){
+    var zero_odd_even = Message.judge_count(count);
+    if (zero_odd_even == "odd") {
+       return(Message.click_start());
+    }
+    if (zero_odd_even == "even") {
+        if (Message.click_end()) {
+            //alert(count);
+           return( "开始");
+        }
+        else {
+            count--;
+            //alert(count);
+            return( "结束");
+        }
+    }
+};
--- app/scripts/controllers/activity_sign_up_controller.js
+++ app/scripts/controllers/activity_sign_up_controller.js
@@ -6,24 +6,11 @@ angular.module('testApp')
         $scope.Back = "返回";
         $scope.Start = "开始";
         $scope.back_to_activity_list = function () {
-            $location.path('/activity_list')
+               $location.path('/activity_list')
         };
         $scope.click_start_button = function () {
             count++;
-            var zero_odd_even = Message.judge_count(count);
-            if (zero_odd_even == "odd") {
-                $scope.Start = Message.click_start();
-            }
-            if (zero_odd_even == "even") {
-                if (Message.click_end()) {
-                    $scope.Start = "开始";
-                    //alert(count);
-                }
-                else {
-                    count--;
-                    //alert(count);
-                }
-            }
+            $scope.Start = Message.button(count);
         }
     });
 
