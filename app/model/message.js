cab8c87329fd17b9feba81dcc9c537596118773e
tiantian-yuqing <501873986@qq.com>
2014-08-07 20:33:03 -0400

    统计鼠标点击开始按钮的次数

---
+++ app/model/message.js
@@ -0,0 +1,21 @@
+/**
+ * Created by tiantian on 14-8-7.
+ */
+/*function Message(name,phone){
+    this.name = name;
+    this.phone = phone;
+}
+
+Message.click_start = function(start){
+    start = "结束"
+
+};
+
+Message.click_end = function(end){
+    var confirm_end = confrim("是否结束本次报名")
+    if (confirm_end){
+        count--;
+        end = "开始" ;
+    }
+}
+*/
--- app/scripts/controllers/activity_sign_up_controller.js
+++ app/scripts/controllers/activity_sign_up_controller.js
@@ -2,9 +2,14 @@
 
 angular.module('testApp')
     .controller('activitySignUpCtrl', function ($scope, $location)
-    {
+    {    var count = 0 ;
          $scope.Back="返回";
          $scope.Start="开始";
          $scope.back_to_activity_list=function(){$location.path('/activity_list')};
-
+         $scope.click_number=function(){
+             count++; alert(count);
+         }
     });
+
+
+
