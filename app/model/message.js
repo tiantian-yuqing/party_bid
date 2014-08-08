1851400daf72b21e76c4ec557acacb7734304254
tiantian-yuqing <501873986@qq.com>
2014-08-07 23:23:14 -0400

    实现结束按钮的全部功能和开始按钮的部分功能

--- app/model/message.js
+++ app/model/message.js
@@ -6,20 +6,16 @@ function Message(name,phone){
     this.phone = phone;
 }
 
-/*Message.click_start = function(start){
-    start = "结束"
+Message.click_start = function(){
+    return("结束");
 
 };
 
-Message.click_end = function(end){
-    var confirm_end = confrim("是否结束本次报名");
-    if (confirm_end){
-        count-- ;
-        end = "开始" ;
-    }
+Message.click_end = function(count){
+        return(confirm("是否结束本次报名?"));
 };
-*/
 
 Message.judge_count=function(count) {
     return((count == 0)?"zero" :(count % 2 == 0) ? "even" : "odd");
 };
+
--- app/scripts/controllers/activity_sign_up_controller.js
+++ app/scripts/controllers/activity_sign_up_controller.js
@@ -1,36 +1,57 @@
 'use strict';
 
 angular.module('testApp')
-    .controller('activitySignUpCtrl', function ($scope, $location)
-    {    var count = 0 ;
-         $scope.Back="返回";
-         $scope.Start="开始";
-         $scope.back_to_activity_list=function(){$location.path('/activity_list')};
-         $scope.click_number=function(){
-             count++; alert(count);
-            alert(Message.judge_count(count));
-         }
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    .controller('activitySignUpCtrl', function ($scope, $location) {
+        var count = 0;
+        $scope.Back = "返回";
+        $scope.Start = "开始";
+        $scope.back_to_activity_list = function () {
+            $location.path('/activity_list')
+        };
+        $scope.click_start_button = function () {
+            count++;
+            var zero_odd_even = Message.judge_count(count);
+            if (zero_odd_even == "odd") {
+                $scope.Start = Message.click_start();
+            }
+            if (zero_odd_even == "even") {
+                if (Message.click_end()) {
+                    $scope.Start = "开始";
+                    //alert(count);
+                }
+                else {
+                    count--;
+                    //alert(count);
+                }
+            }
+        }
     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
